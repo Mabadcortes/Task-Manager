a394b9d8066d2006de8e9a19e3d6e388dfc07b2c
Mabadcortes <rebelliongames12@gmail.com>
2026-09-15 15:51:07 +0100

    feat: add DTOs, mapper, global exception handling and validations

--- taskmanager/src/main/java/com/mabadcortes/taskmanager/dto/TaskRequestDTO.java
+++ taskmanager/src/main/java/com/mabadcortes/taskmanager/dto/TaskRequestDTO.java
@@ -1,6 +1,7 @@
 package com.mabadcortes.taskmanager.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,15 +12,19 @@ public class TaskRequestDTO {
     /*
      * Title received from the client.
      * It cannot be null, empty or blank.
+     * Must be between 3 and 100 characters.
      */
     @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
     private String title;
 
     /*
      * Description received from the client.
      * It cannot be null, empty or blank.
+     * Maximum 500 characters.
      */
     @NotBlank(message = "Description is required")
+    @Size(max = 500, message = "Description cannot exceed 500 characters")
     private String description;
 
     /*
